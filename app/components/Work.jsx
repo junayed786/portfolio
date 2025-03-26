17af33dd27f2d8597489fea0b57d33ee3818ecb4
Junayed <junayed0786@gmail.com>
2025-03-26 03:55:56 +0100

    Add d3-solana link

--- app/components/Work.jsx
+++ app/components/Work.jsx
@@ -39,12 +39,12 @@ const Work = () => {
             </a>
             ))}
         </div>
-        {/* <a href="" className='w-max flex items-center justify-center gap-2 
+        <a href="http://solana-graph-d3.s3-website.eu-north-1.amazonaws.com/" className='w-max flex items-center justify-center gap-2 
         text-gray-700 border-[0.5px] border-gray-700 rounded-full py-3 px-10 mx-auto
         my-20 hover:bg-lightHover duration-500'>
             show more <Image src={assets.right_arrow_bold} alt='Right arrow'
             className='w-4'/>
-        </a> */}
+        </a>
     </div>
   )
 }
